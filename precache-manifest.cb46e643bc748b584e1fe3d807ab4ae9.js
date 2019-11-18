3f19101d59f9b8f4885cdf0fae4e9bb94209a6b2
aleksioconnor <aleksi.oconnor@gmail.com>
2019-11-18 17:54:27 +0200

    Updates

---
+++ precache-manifest.cb46e643bc748b584e1fe3d807ab4ae9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ccbe456c786b415e14d8f556f42f318d",
+    "url": "/recipegen/index.html"
+  },
+  {
+    "revision": "8994717942effdf03380",
+    "url": "/recipegen/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "cd0f1940dc7a3084ad0b",
+    "url": "/recipegen/static/css/main.b35b2d1f.chunk.css"
+  },
+  {
+    "revision": "8994717942effdf03380",
+    "url": "/recipegen/static/js/2.ae84fe66.chunk.js"
+  },
+  {
+    "revision": "cd0f1940dc7a3084ad0b",
+    "url": "/recipegen/static/js/main.0cbd8218.chunk.js"
+  },
+  {
+    "revision": "09e56bfef3810a9723be",
+    "url": "/recipegen/static/js/runtime-main.49701214.js"
+  }
+]);
